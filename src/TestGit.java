62d4df002cf707d87861a4b1d3d3c4595fcaa3d0
vinod <vinod@DESKTOP-7SF5PLV>
2023-08-08 16:57:38 +0530

    testfile commit

--- src/TestGit.java
+++ src/TestGit.java
@@ -2,8 +2,8 @@
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("welcome git");
+		
+            System.out.println("welcome git");
 	}
 
 }
